dc2c3fc47c4631312f97f59cae7dfb0ebfd58d57
kmchord9 <kmchord9@gmail.com>
2023-06-23 09:46:31 +0900

    volumeフォルダにソースファイル移行

--- volume/node/index.js
+++ volume/node/index.js
@@ -1,10 +1,13 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', (socket) => {
